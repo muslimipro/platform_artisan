b7a9ff6c09b58258209ee58314b42eea6cee7326
Yertugan <muslim.ipro@gmail.com>
2025-04-14 21:21:43 +0500

    save profile removed

---
+++ assets/index-DUrGE79Z.js
@@ -0,0 +1,98 @@
+import{B as t,o as r,c as i,I as u,x as e,ae as a}from"./index-BiF4ZX3p.js";var s=function(o){var n=o.dt;return`
+.p-inputgroup,
+.p-inputgroup .p-iconfield,
+.p-inputgroup .p-floatlabel,
+.p-inputgroup .p-iftalabel {
+    display: flex;
+    align-items: stretch;
+    width: 100%;
+}
+
+.p-inputgroup .p-inputtext,
+.p-inputgroup .p-inputwrapper {
+    flex: 1 1 auto;
+    width: 1%;
+}
+
+.p-inputgroupaddon {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: `.concat(n("inputgroup.addon.padding"),`;
+    background: `).concat(n("inputgroup.addon.background"),`;
+    color: `).concat(n("inputgroup.addon.color"),`;
+    border-block-start: 1px solid `).concat(n("inputgroup.addon.border.color"),`;
+    border-block-end: 1px solid `).concat(n("inputgroup.addon.border.color"),`;
+    min-width: `).concat(n("inputgroup.addon.min.width"),`;
+}
+
+.p-inputgroupaddon:first-child,
+.p-inputgroupaddon + .p-inputgroupaddon {
+    border-inline-start: 1px solid `).concat(n("inputgroup.addon.border.color"),`;
+}
+
+.p-inputgroupaddon:last-child {
+    border-inline-end: 1px solid `).concat(n("inputgroup.addon.border.color"),`;
+}
+
+.p-inputgroupaddon:has(.p-button) {
+    padding: 0;
+    overflow: hidden;
+}
+
+.p-inputgroupaddon .p-button {
+    border-radius: 0;
+}
+
+.p-inputgroup > .p-component,
+.p-inputgroup > .p-inputwrapper > .p-component,
+.p-inputgroup > .p-iconfield > .p-component,
+.p-inputgroup > .p-floatlabel > .p-component,
+.p-inputgroup > .p-floatlabel > .p-inputwrapper > .p-component,
+.p-inputgroup > .p-iftalabel > .p-component,
+.p-inputgroup > .p-iftalabel > .p-inputwrapper > .p-component {
+    border-radius: 0;
+    margin: 0;
+}
+
+.p-inputgroupaddon:first-child,
+.p-inputgroup > .p-component:first-child,
+.p-inputgroup > .p-inputwrapper:first-child > .p-component,
+.p-inputgroup > .p-iconfield:first-child > .p-component,
+.p-inputgroup > .p-floatlabel:first-child > .p-component,
+.p-inputgroup > .p-floatlabel:first-child > .p-inputwrapper > .p-component,
+.p-inputgroup > .p-iftalabel:first-child > .p-component,
+.p-inputgroup > .p-iftalabel:first-child > .p-inputwrapper > .p-component {
+    border-start-start-radius: `).concat(n("inputgroup.addon.border.radius"),`;
+    border-end-start-radius: `).concat(n("inputgroup.addon.border.radius"),`;
+}
+
+.p-inputgroupaddon:last-child,
+.p-inputgroup > .p-component:last-child,
+.p-inputgroup > .p-inputwrapper:last-child > .p-component,
+.p-inputgroup > .p-iconfield:last-child > .p-component,
+.p-inputgroup > .p-floatlabel:last-child > .p-component,
+.p-inputgroup > .p-floatlabel:last-child > .p-inputwrapper > .p-component,
+.p-inputgroup > .p-iftalabel:last-child > .p-component,
+.p-inputgroup > .p-iftalabel:last-child > .p-inputwrapper > .p-component {
+    border-start-end-radius: `).concat(n("inputgroup.addon.border.radius"),`;
+    border-end-end-radius: `).concat(n("inputgroup.addon.border.radius"),`;
+}
+
+.p-inputgroup .p-component:focus,
+.p-inputgroup .p-component.p-focus,
+.p-inputgroup .p-inputwrapper-focus,
+.p-inputgroup .p-component:focus ~ label,
+.p-inputgroup .p-component.p-focus ~ label,
+.p-inputgroup .p-inputwrapper-focus ~ label {
+    z-index: 1;
+}
+
+.p-inputgroup > .p-button:not(.p-button-icon-only) {
+    width: auto;
+}
+
+.p-inputgroup .p-iconfield + .p-iconfield .p-inputtext {
+    border-inline-start: 0;
+}
+`)},g={root:"p-inputgroup"},f=t.extend({name:"inputgroup",theme:s,classes:g}),m={name:"BaseInputGroup",extends:a,style:f,provide:function(){return{$pcInputGroup:this,$parentInstance:this}}},b={name:"InputGroup",extends:m,inheritAttrs:!1};function h(p,o,n,d,c,l){return r(),i("div",e({class:p.cx("root")},p.ptmi("root")),[u(p.$slots,"default")],16)}b.render=h;var $={root:"p-inputgroupaddon"},w=t.extend({name:"inputgroupaddon",classes:$}),v={name:"BaseInputGroupAddon",extends:a,style:w,provide:function(){return{$pcInputGroupAddon:this,$parentInstance:this}}},x={name:"InputGroupAddon",extends:v,inheritAttrs:!1};function I(p,o,n,d,c,l){return r(),i("div",e({class:p.cx("root")},p.ptmi("root")),[u(p.$slots,"default")],16)}x.render=I;export{b as a,x as s};
