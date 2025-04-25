8bc3fdc101443748dbbd0ba63645107546d3c481
Yertugan <muslim.ipro@gmail.com>
2025-04-25 22:32:07 +0500

    layers fixes

---
+++ assets/index-BXoADN-d.js
@@ -0,0 +1,275 @@
+import{B as s,ar as h,bu as g,aI as m,H as b,o as d,c as p,e as r,x as a,I as v,f as l,h as y,aj as B}from"./index-BpBbGszO.js";import{s as w}from"./index-m8-w2RQ0.js";import{a as k}from"./index-DlipmIb-.js";var C=function(e){var n=e.dt;return`
+.p-checkbox {
+    position: relative;
+    display: inline-flex;
+    user-select: none;
+    vertical-align: bottom;
+    width: `.concat(n("checkbox.width"),`;
+    height: `).concat(n("checkbox.height"),`;
+}
+
+.p-checkbox-input {
+    cursor: pointer;
+    appearance: none;
+    position: absolute;
+    inset-block-start: 0;
+    inset-inline-start: 0;
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    opacity: 0;
+    z-index: 1;
+    outline: 0 none;
+    border: 1px solid transparent;
+    border-radius: `).concat(n("checkbox.border.radius"),`;
+}
+
+.p-checkbox-box {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: `).concat(n("checkbox.border.radius"),`;
+    border: 1px solid `).concat(n("checkbox.border.color"),`;
+    background: `).concat(n("checkbox.background"),`;
+    width: `).concat(n("checkbox.width"),`;
+    height: `).concat(n("checkbox.height"),`;
+    transition: background `).concat(n("checkbox.transition.duration"),", color ").concat(n("checkbox.transition.duration"),", border-color ").concat(n("checkbox.transition.duration"),", box-shadow ").concat(n("checkbox.transition.duration"),", outline-color ").concat(n("checkbox.transition.duration"),`;
+    outline-color: transparent;
+    box-shadow: `).concat(n("checkbox.shadow"),`;
+}
+
+.p-checkbox-icon {
+    transition-duration: `).concat(n("checkbox.transition.duration"),`;
+    color: `).concat(n("checkbox.icon.color"),`;
+    font-size: `).concat(n("checkbox.icon.size"),`;
+    width: `).concat(n("checkbox.icon.size"),`;
+    height: `).concat(n("checkbox.icon.size"),`;
+}
+
+.p-checkbox:not(.p-disabled):has(.p-checkbox-input:hover) .p-checkbox-box {
+    border-color: `).concat(n("checkbox.hover.border.color"),`;
+}
+
+.p-checkbox-checked .p-checkbox-box {
+    border-color: `).concat(n("checkbox.checked.border.color"),`;
+    background: `).concat(n("checkbox.checked.background"),`;
+}
+
+.p-checkbox-checked .p-checkbox-icon {
+    color: `).concat(n("checkbox.icon.checked.color"),`;
+}
+
+.p-checkbox-checked:not(.p-disabled):has(.p-checkbox-input:hover) .p-checkbox-box {
+    background: `).concat(n("checkbox.checked.hover.background"),`;
+    border-color: `).concat(n("checkbox.checked.hover.border.color"),`;
+}
+
+.p-checkbox-checked:not(.p-disabled):has(.p-checkbox-input:hover) .p-checkbox-icon {
+    color: `).concat(n("checkbox.icon.checked.hover.color"),`;
+}
+
+.p-checkbox:not(.p-disabled):has(.p-checkbox-input:focus-visible) .p-checkbox-box {
+    border-color: `).concat(n("checkbox.focus.border.color"),`;
+    box-shadow: `).concat(n("checkbox.focus.ring.shadow"),`;
+    outline: `).concat(n("checkbox.focus.ring.width")," ").concat(n("checkbox.focus.ring.style")," ").concat(n("checkbox.focus.ring.color"),`;
+    outline-offset: `).concat(n("checkbox.focus.ring.offset"),`;
+}
+
+.p-checkbox-checked:not(.p-disabled):has(.p-checkbox-input:focus-visible) .p-checkbox-box {
+    border-color: `).concat(n("checkbox.checked.focus.border.color"),`;
+}
+
+.p-checkbox.p-invalid > .p-checkbox-box {
+    border-color: `).concat(n("checkbox.invalid.border.color"),`;
+}
+
+.p-checkbox.p-variant-filled .p-checkbox-box {
+    background: `).concat(n("checkbox.filled.background"),`;
+}
+
+.p-checkbox-checked.p-variant-filled .p-checkbox-box {
+    background: `).concat(n("checkbox.checked.background"),`;
+}
+
+.p-checkbox-checked.p-variant-filled:not(.p-disabled):has(.p-checkbox-input:hover) .p-checkbox-box {
+    background: `).concat(n("checkbox.checked.hover.background"),`;
+}
+
+.p-checkbox.p-disabled {
+    opacity: 1;
+}
+
+.p-checkbox.p-disabled .p-checkbox-box {
+    background: `).concat(n("checkbox.disabled.background"),`;
+    border-color: `).concat(n("checkbox.checked.disabled.border.color"),`;
+}
+
+.p-checkbox.p-disabled .p-checkbox-box .p-checkbox-icon {
+    color: `).concat(n("checkbox.icon.disabled.color"),`;
+}
+
+.p-checkbox-sm,
+.p-checkbox-sm .p-checkbox-box {
+    width: `).concat(n("checkbox.sm.width"),`;
+    height: `).concat(n("checkbox.sm.height"),`;
+}
+
+.p-checkbox-sm .p-checkbox-icon {
+    font-size: `).concat(n("checkbox.icon.sm.size"),`;
+    width: `).concat(n("checkbox.icon.sm.size"),`;
+    height: `).concat(n("checkbox.icon.sm.size"),`;
+}
+
+.p-checkbox-lg,
+.p-checkbox-lg .p-checkbox-box {
+    width: `).concat(n("checkbox.lg.width"),`;
+    height: `).concat(n("checkbox.lg.height"),`;
+}
+
+.p-checkbox-lg .p-checkbox-icon {
+    font-size: `).concat(n("checkbox.icon.lg.size"),`;
+    width: `).concat(n("checkbox.icon.lg.size"),`;
+    height: `).concat(n("checkbox.icon.lg.size"),`;
+}
+`)},z={root:function(e){var n=e.instance,c=e.props;return["p-checkbox p-component",{"p-checkbox-checked":n.checked,"p-disabled":c.disabled,"p-invalid":n.$pcCheckboxGroup?n.$pcCheckboxGroup.$invalid:n.$invalid,"p-variant-filled":n.$variant==="filled","p-checkbox-sm p-inputfield-sm":c.size==="small","p-checkbox-lg p-inputfield-lg":c.size==="large"}]},box:"p-checkbox-box",input:"p-checkbox-input",icon:"p-checkbox-icon"},$=s.extend({name:"checkbox",theme:C,classes:z}),S={name:"BaseCheckbox",extends:k,props:{value:null,binary:Boolean,indeterminate:{type:Boolean,default:!1},trueValue:{type:null,default:!0},falseValue:{type:null,default:!1},readonly:{type:Boolean,default:!1},required:{type:Boolean,default:!1},tabindex:{type:Number,default:null},inputId:{type:String,default:null},inputClass:{type:[String,Object],default:null},inputStyle:{type:Object,default:null},ariaLabelledby:{type:String,default:null},ariaLabel:{type:String,default:null}},style:$,provide:function(){return{$pcCheckbox:this,$parentInstance:this}}};function G(o){return F(o)||T(o)||O(o)||I()}function I(){throw new TypeError(`Invalid attempt to spread non-iterable instance.
+In order to be iterable, non-array objects must have a [Symbol.iterator]() method.`)}function O(o,e){if(o){if(typeof o=="string")return u(o,e);var n={}.toString.call(o).slice(8,-1);return n==="Object"&&o.constructor&&(n=o.constructor.name),n==="Map"||n==="Set"?Array.from(o):n==="Arguments"||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)?u(o,e):void 0}}function T(o){if(typeof Symbol<"u"&&o[Symbol.iterator]!=null||o["@@iterator"]!=null)return Array.from(o)}function F(o){if(Array.isArray(o))return u(o)}function u(o,e){(e==null||e>o.length)&&(e=o.length);for(var n=0,c=Array(e);n<e;n++)c[n]=o[n];return c}var N={name:"Checkbox",extends:S,inheritAttrs:!1,emits:["change","focus","blur","update:indeterminate"],inject:{$pcCheckboxGroup:{default:void 0}},data:function(){return{d_indeterminate:this.indeterminate}},watch:{indeterminate:function(e){this.d_indeterminate=e}},methods:{getPTOptions:function(e){var n=e==="root"?this.ptmi:this.ptm;return n(e,{context:{checked:this.checked,indeterminate:this.d_indeterminate,disabled:this.disabled}})},onChange:function(e){var n=this;if(!this.disabled&&!this.readonly){var c=this.$pcCheckboxGroup?this.$pcCheckboxGroup.d_value:this.d_value,i;this.binary?i=this.d_indeterminate?this.trueValue:this.checked?this.falseValue:this.trueValue:this.checked||this.d_indeterminate?i=c.filter(function(t){return!h(t,n.value)}):i=c?[].concat(G(c),[this.value]):[this.value],this.d_indeterminate&&(this.d_indeterminate=!1,this.$emit("update:indeterminate",this.d_indeterminate)),this.$pcCheckboxGroup?this.$pcCheckboxGroup.writeValue(i,e):this.writeValue(i,e),this.$emit("change",e)}},onFocus:function(e){this.$emit("focus",e)},onBlur:function(e){var n,c;this.$emit("blur",e),(n=(c=this.formField).onBlur)===null||n===void 0||n.call(c,e)}},computed:{groupName:function(){return this.$pcCheckboxGroup?this.$pcCheckboxGroup.groupName:this.$formName},checked:function(){var e=this.$pcCheckboxGroup?this.$pcCheckboxGroup.d_value:this.d_value;return this.d_indeterminate?!1:this.binary?e===this.trueValue:g(this.value,e)}},components:{CheckIcon:m,MinusIcon:w}},P=["data-p-checked","data-p-indeterminate","data-p-disabled"],A=["id","value","name","checked","tabindex","disabled","readonly","required","aria-labelledby","aria-label","aria-invalid","aria-checked"];function R(o,e,n,c,i,t){var x=b("CheckIcon"),f=b("MinusIcon");return d(),p("div",a({class:o.cx("root")},t.getPTOptions("root"),{"data-p-checked":t.checked,"data-p-indeterminate":i.d_indeterminate||void 0,"data-p-disabled":o.disabled}),[r("input",a({id:o.inputId,type:"checkbox",class:[o.cx("input"),o.inputClass],style:o.inputStyle,value:o.value,name:t.groupName,checked:t.checked,tabindex:o.tabindex,disabled:o.disabled,readonly:o.readonly,required:o.required,"aria-labelledby":o.ariaLabelledby,"aria-label":o.ariaLabel,"aria-invalid":o.invalid||void 0,"aria-checked":i.d_indeterminate?"mixed":void 0,onFocus:e[0]||(e[0]=function(){return t.onFocus&&t.onFocus.apply(t,arguments)}),onBlur:e[1]||(e[1]=function(){return t.onBlur&&t.onBlur.apply(t,arguments)}),onChange:e[2]||(e[2]=function(){return t.onChange&&t.onChange.apply(t,arguments)})},t.getPTOptions("input")),null,16,A),r("div",a({class:o.cx("box")},t.getPTOptions("box")),[v(o.$slots,"icon",{checked:t.checked,indeterminate:i.d_indeterminate,class:B(o.cx("icon"))},function(){return[t.checked?(d(),l(x,a({key:0,class:o.cx("icon")},t.getPTOptions("icon")),null,16,["class"])):i.d_indeterminate?(d(),l(f,a({key:1,class:o.cx("icon")},t.getPTOptions("icon")),null,16,["class"])):y("",!0)]})],16)],16,P)}N.render=R;var j=function(e){var n=e.dt;return`
+.p-radiobutton {
+    position: relative;
+    display: inline-flex;
+    user-select: none;
+    vertical-align: bottom;
+    width: `.concat(n("radiobutton.width"),`;
+    height: `).concat(n("radiobutton.height"),`;
+}
+
+.p-radiobutton-input {
+    cursor: pointer;
+    appearance: none;
+    position: absolute;
+    top: 0;
+    inset-inline-start: 0;
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    opacity: 0;
+    z-index: 1;
+    outline: 0 none;
+    border: 1px solid transparent;
+    border-radius: 50%;
+}
+
+.p-radiobutton-box {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    border: 1px solid `).concat(n("radiobutton.border.color"),`;
+    background: `).concat(n("radiobutton.background"),`;
+    width: `).concat(n("radiobutton.width"),`;
+    height: `).concat(n("radiobutton.height"),`;
+    transition: background `).concat(n("radiobutton.transition.duration"),", color ").concat(n("radiobutton.transition.duration"),", border-color ").concat(n("radiobutton.transition.duration"),", box-shadow ").concat(n("radiobutton.transition.duration"),", outline-color ").concat(n("radiobutton.transition.duration"),`;
+    outline-color: transparent;
+    box-shadow: `).concat(n("radiobutton.shadow"),`;
+}
+
+.p-radiobutton-icon {
+    transition-duration: `).concat(n("radiobutton.transition.duration"),`;
+    background: transparent;
+    font-size: `).concat(n("radiobutton.icon.size"),`;
+    width: `).concat(n("radiobutton.icon.size"),`;
+    height: `).concat(n("radiobutton.icon.size"),`;
+    border-radius: 50%;
+    backface-visibility: hidden;
+    transform: translateZ(0) scale(0.1);
+}
+
+.p-radiobutton:not(.p-disabled):has(.p-radiobutton-input:hover) .p-radiobutton-box {
+    border-color: `).concat(n("radiobutton.hover.border.color"),`;
+}
+
+.p-radiobutton-checked .p-radiobutton-box {
+    border-color: `).concat(n("radiobutton.checked.border.color"),`;
+    background: `).concat(n("radiobutton.checked.background"),`;
+}
+
+.p-radiobutton-checked .p-radiobutton-box .p-radiobutton-icon {
+    background: `).concat(n("radiobutton.icon.checked.color"),`;
+    transform: translateZ(0) scale(1, 1);
+    visibility: visible;
+}
+
+.p-radiobutton-checked:not(.p-disabled):has(.p-radiobutton-input:hover) .p-radiobutton-box {
+    border-color: `).concat(n("radiobutton.checked.hover.border.color"),`;
+    background: `).concat(n("radiobutton.checked.hover.background"),`;
+}
+
+.p-radiobutton:not(.p-disabled):has(.p-radiobutton-input:hover).p-radiobutton-checked .p-radiobutton-box .p-radiobutton-icon {
+    background: `).concat(n("radiobutton.icon.checked.hover.color"),`;
+}
+
+.p-radiobutton:not(.p-disabled):has(.p-radiobutton-input:focus-visible) .p-radiobutton-box {
+    border-color: `).concat(n("radiobutton.focus.border.color"),`;
+    box-shadow: `).concat(n("radiobutton.focus.ring.shadow"),`;
+    outline: `).concat(n("radiobutton.focus.ring.width")," ").concat(n("radiobutton.focus.ring.style")," ").concat(n("radiobutton.focus.ring.color"),`;
+    outline-offset: `).concat(n("radiobutton.focus.ring.offset"),`;
+}
+
+.p-radiobutton-checked:not(.p-disabled):has(.p-radiobutton-input:focus-visible) .p-radiobutton-box {
+    border-color: `).concat(n("radiobutton.checked.focus.border.color"),`;
+}
+
+.p-radiobutton.p-invalid > .p-radiobutton-box {
+    border-color: `).concat(n("radiobutton.invalid.border.color"),`;
+}
+
+.p-radiobutton.p-variant-filled .p-radiobutton-box {
+    background: `).concat(n("radiobutton.filled.background"),`;
+}
+
+.p-radiobutton.p-variant-filled.p-radiobutton-checked .p-radiobutton-box {
+    background: `).concat(n("radiobutton.checked.background"),`;
+}
+
+.p-radiobutton.p-variant-filled:not(.p-disabled):has(.p-radiobutton-input:hover).p-radiobutton-checked .p-radiobutton-box {
+    background: `).concat(n("radiobutton.checked.hover.background"),`;
+}
+
+.p-radiobutton.p-disabled {
+    opacity: 1;
+}
+
+.p-radiobutton.p-disabled .p-radiobutton-box {
+    background: `).concat(n("radiobutton.disabled.background"),`;
+    border-color: `).concat(n("radiobutton.checked.disabled.border.color"),`;
+}
+
+.p-radiobutton-checked.p-disabled .p-radiobutton-box .p-radiobutton-icon {
+    background: `).concat(n("radiobutton.icon.disabled.color"),`;
+}
+
+.p-radiobutton-sm,
+.p-radiobutton-sm .p-radiobutton-box {
+    width: `).concat(n("radiobutton.sm.width"),`;
+    height: `).concat(n("radiobutton.sm.height"),`;
+}
+
+.p-radiobutton-sm .p-radiobutton-icon {
+    font-size: `).concat(n("radiobutton.icon.sm.size"),`;
+    width: `).concat(n("radiobutton.icon.sm.size"),`;
+    height: `).concat(n("radiobutton.icon.sm.size"),`;
+}
+
+.p-radiobutton-lg,
+.p-radiobutton-lg .p-radiobutton-box {
+    width: `).concat(n("radiobutton.lg.width"),`;
+    height: `).concat(n("radiobutton.lg.height"),`;
+}
+
+.p-radiobutton-lg .p-radiobutton-icon {
+    font-size: `).concat(n("radiobutton.icon.lg.size"),`;
+    width: `).concat(n("radiobutton.icon.lg.size"),`;
+    height: `).concat(n("radiobutton.icon.lg.size"),`;
+}
+`)},L={root:function(e){var n=e.instance,c=e.props;return["p-radiobutton p-component",{"p-radiobutton-checked":n.checked,"p-disabled":c.disabled,"p-invalid":n.$pcRadioButtonGroup?n.$pcRadioButtonGroup.$invalid:n.$invalid,"p-variant-filled":n.$variant==="filled","p-radiobutton-sm p-inputfield-sm":c.size==="small","p-radiobutton-lg p-inputfield-lg":c.size==="large"}]},box:"p-radiobutton-box",input:"p-radiobutton-input",icon:"p-radiobutton-icon"},V=s.extend({name:"radiobutton",theme:j,classes:L}),q={name:"BaseRadioButton",extends:k,props:{value:null,binary:Boolean,readonly:{type:Boolean,default:!1},tabindex:{type:Number,default:null},inputId:{type:String,default:null},inputClass:{type:[String,Object],default:null},inputStyle:{type:Object,default:null},ariaLabelledby:{type:String,default:null},ariaLabel:{type:String,default:null}},style:V,provide:function(){return{$pcRadioButton:this,$parentInstance:this}}},E={name:"RadioButton",extends:q,inheritAttrs:!1,emits:["change","focus","blur"],inject:{$pcRadioButtonGroup:{default:void 0}},methods:{getPTOptions:function(e){var n=e==="root"?this.ptmi:this.ptm;return n(e,{context:{checked:this.checked,disabled:this.disabled}})},onChange:function(e){if(!this.disabled&&!this.readonly){var n=this.binary?!this.checked:this.value;this.$pcRadioButtonGroup?this.$pcRadioButtonGroup.writeValue(n,e):this.writeValue(n,e),this.$emit("change",e)}},onFocus:function(e){this.$emit("focus",e)},onBlur:function(e){var n,c;this.$emit("blur",e),(n=(c=this.formField).onBlur)===null||n===void 0||n.call(c,e)}},computed:{groupName:function(){return this.$pcRadioButtonGroup?this.$pcRadioButtonGroup.groupName:this.$formName},checked:function(){var e=this.$pcRadioButtonGroup?this.$pcRadioButtonGroup.d_value:this.d_value;return e!=null&&(this.binary?!!e:h(e,this.value))}}},H=["data-p-checked","data-p-disabled"],Z=["id","value","name","checked","tabindex","disabled","readonly","aria-labelledby","aria-label","aria-invalid"];function U(o,e,n,c,i,t){return d(),p("div",a({class:o.cx("root")},t.getPTOptions("root"),{"data-p-checked":t.checked,"data-p-disabled":o.disabled}),[r("input",a({id:o.inputId,type:"radio",class:[o.cx("input"),o.inputClass],style:o.inputStyle,value:o.value,name:t.groupName,checked:t.checked,tabindex:o.tabindex,disabled:o.disabled,readonly:o.readonly,"aria-labelledby":o.ariaLabelledby,"aria-label":o.ariaLabel,"aria-invalid":o.invalid||void 0,onFocus:e[0]||(e[0]=function(){return t.onFocus&&t.onFocus.apply(t,arguments)}),onBlur:e[1]||(e[1]=function(){return t.onBlur&&t.onBlur.apply(t,arguments)}),onChange:e[2]||(e[2]=function(){return t.onChange&&t.onChange.apply(t,arguments)})},t.getPTOptions("input")),null,16,Z),r("div",a({class:o.cx("box")},t.getPTOptions("box")),[r("div",a({class:o.cx("icon")},t.getPTOptions("icon")),null,16)],16)],16,H)}E.render=U;export{E as a,N as s};
