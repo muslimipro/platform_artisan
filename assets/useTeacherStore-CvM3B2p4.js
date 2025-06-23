f2c6773a6b698d4ad3befee83ae6d0d49de2a313
Yertugan <muslim.ipro@gmail.com>
2025-06-23 23:31:32 +0500

    hotfix-submit added

---
+++ assets/useTeacherStore-CvM3B2p4.js
@@ -0,0 +1,1 @@
+import{as as o,r}from"./index-CFTsbYyj.js";import{T as l}from"./teacherApi-DcrOEcun.js";const i=o("useTeacherStore",()=>{const e=r([]),a=r(!1);async function t(){try{a.value=!0;const{data:s}=await l.getClasses();s?e.value=s:e.value=[]}finally{a.value=!1}}return{classes:e,isLoading:a,loadClasses:t}});export{i as u};
