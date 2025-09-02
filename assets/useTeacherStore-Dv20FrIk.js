30c358651a1eb35639920ba2fb0794990a79e671
Yertugan <muslim.ipro@gmail.com>
2025-09-02 15:31:17 +0500

    lego unclocked

---
+++ assets/useTeacherStore-Dv20FrIk.js
@@ -0,0 +1,1 @@
+import{d as o,r}from"./index-6znFarQA.js";import{T as l}from"./teacherApi-CGSqW9gM.js";const i=o("useTeacherStore",()=>{const e=r([]),a=r(!1);async function t(){try{a.value=!0;const{data:s}=await l.getClasses();s?e.value=s:e.value=[]}finally{a.value=!1}}return{classes:e,isLoading:a,loadClasses:t}});export{i as u};
